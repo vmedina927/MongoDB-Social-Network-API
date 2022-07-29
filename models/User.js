3646edc64d27719d3d9584f9986542d97e8c830c
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-29 19:08:25 -0400

    Added code for Thought.js and User.js

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+\@.+\..+/],
+        },
+
+        thought: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
