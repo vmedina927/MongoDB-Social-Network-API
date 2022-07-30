606460a6bc09a236bf5dc07fc7da8f0bd7891678
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-29 21:06:40 -0400

    Fixed errors in some files

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router;
-const { create } = require('domain');
 const {
     getAllUsers,
     getUserById,
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mongodb-social-network-api', {
     useNewUrlParser: true,
@@ -15,7 +15,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mongodb-s
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
-
-app.use(require('.routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
